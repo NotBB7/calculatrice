c67842864a3f559ab0b9b3c578b71473b68914c7
NotBB <notice76610@gmail.com>
2023-05-09 14:48:47 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 function appendToDisplay(value) {
+    // Ajoute la valeur au contenu de l'affichage
     document.getElementById('display').value += value;
 }
 
 function clearDisplay() {
+    // Efface le contenu de l'affichage
     document.getElementById('display').value = '';
 }
 
 function calculateResult() {
+    // Récupère la valeur affichée dans l'affichage
     var displayValue = document.getElementById('display').value;
+    // Évalue l'expression mathématique et obtient le résultat
     var result = eval(displayValue);
+    // Affiche le résultat dans l'affichage
     document.getElementById('display').value = result;
 }
